d993f4291ffd656a22168676f61e5b7416aaae37
f550971 <f550971@corp.caixa.gov.br>
2020-07-22 23:40:21 -0300

    Criando classes do pacote tabuleiro

--- src/application/ExecutaTabuleiro.java
+++ src/application/ExecutaTabuleiro.java
@@ -1,12 +1,15 @@
 package application;
 
 import boardGame.Posicao;
+import boardGame.Tabuleiro;
 
 public class ExecutaTabuleiro {
     public static void main (String[] args) {
 
         Posicao posicao = new Posicao(3,4);
 
-        System.out.println(posicao);
+        Tabuleiro tabuleiro = new Tabuleiro(8,8);
+
+
     }
 }
