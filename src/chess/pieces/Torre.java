074bf7bec94d202bf1657f1c0227d6c5a8ea2e3f
f550971 <f550971@corp.caixa.gov.br>
2020-07-23 23:36:52 -0300

    Metodo de movimentar peças

---
+++ src/chess/pieces/Torre.java
@@ -0,0 +1,17 @@
+package chess.pieces;
+
+import boardGame.Tabuleiro;
+import chess.Cor;
+import chess.PecaXadrez;
+
+public class Torre extends PecaXadrez {
+
+    public Torre(Tabuleiro tabuleiro, Cor cor) {
+        super(tabuleiro, cor);
+    }
+
+    @Override
+    public String toString() {
+        return "T";
+    }
+}
